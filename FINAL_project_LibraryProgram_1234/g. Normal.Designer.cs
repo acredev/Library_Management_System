6a76b3cf4e9c839f8a21d4b08ae4237c475b7294
MinHyuk-Lee <m_file@naver.com>
2022-11-27 21:51:08 +0900

    [DESIGN] 개인 사용자 화면의 디자인을 간결하고 편리하게 아이콘을 제작해 진행했습니다.

--- FINAL_project_LibraryProgram_1234/g. Normal.Designer.cs
+++ FINAL_project_LibraryProgram_1234/g. Normal.Designer.cs
@@ -31,15 +31,22 @@ namespace FINAL_project_LibraryProgram_1234
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Normal));
             this.label5 = new System.Windows.Forms.Label();
-            this.group_mode = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.picture_support = new System.Windows.Forms.PictureBox();
+            this.picture_logout = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.group_mode.SuspendLayout();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_support)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_logout)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,122 +54,169 @@ namespace FINAL_project_LibraryProgram_1234
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label5.Location = new System.Drawing.Point(368, 27);
+            this.label5.Location = new System.Drawing.Point(281, 18);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(321, 50);
+            this.label5.Size = new System.Drawing.Size(258, 40);
             this.label5.TabIndex = 27;
             this.label5.Text = "일이삼사 도서관";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // group_mode
+            // picture_support
             // 
-            this.group_mode.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(185)))), ((int)(((byte)(255)))));
-            this.group_mode.Controls.Add(this.dataGridView1);
-            this.group_mode.Controls.Add(this.button5);
-            this.group_mode.Controls.Add(this.button6);
-            this.group_mode.Controls.Add(this.button3);
-            this.group_mode.Controls.Add(this.button4);
-            this.group_mode.Controls.Add(this.button2);
-            this.group_mode.Controls.Add(this.button1);
-            this.group_mode.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.group_mode.Location = new System.Drawing.Point(12, 97);
-            this.group_mode.Name = "group_mode";
-            this.group_mode.Size = new System.Drawing.Size(1045, 465);
-            this.group_mode.TabIndex = 28;
-            this.group_mode.TabStop = false;
+            this.picture_support.Image = ((System.Drawing.Image)(resources.GetObject("picture_support.Image")));
+            this.picture_support.Location = new System.Drawing.Point(679, 9);
+            this.picture_support.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.picture_support.Name = "picture_support";
+            this.picture_support.Size = new System.Drawing.Size(47, 56);
+            this.picture_support.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture_support.TabIndex = 31;
+            this.picture_support.TabStop = false;
             // 
-            // dataGridView1
+            // picture_logout
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(352, 49);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 6;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.picture_logout.Image = ((System.Drawing.Image)(resources.GetObject("picture_logout.Image")));
+            this.picture_logout.Location = new System.Drawing.Point(749, 9);
+            this.picture_logout.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.picture_logout.Name = "picture_logout";
+            this.picture_logout.Size = new System.Drawing.Size(42, 56);
+            this.picture_logout.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture_logout.TabIndex = 30;
+            this.picture_logout.TabStop = false;
             // 
-            // button5
+            // button1
             // 
-            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
-            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button5.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.button5.Location = new System.Drawing.Point(327, 321);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(179, 94);
-            this.button5.TabIndex = 5;
-            this.button5.UseVisualStyleBackColor = true;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(16, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 122);
+            this.button1.TabIndex = 32;
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // groupBox1
             // 
-            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
-            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button6.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.button6.Location = new System.Drawing.Point(96, 321);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(179, 94);
-            this.button6.TabIndex = 4;
-            this.button6.UseVisualStyleBackColor = true;
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 277);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(779, 290);
+            this.groupBox1.TabIndex = 33;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(276, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(230, 122);
+            this.button2.TabIndex = 33;
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
             this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
             this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button3.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.button3.Location = new System.Drawing.Point(327, 190);
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(534, 22);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(179, 94);
-            this.button3.TabIndex = 3;
+            this.button3.Size = new System.Drawing.Size(230, 122);
+            this.button3.TabIndex = 34;
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
             this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button4.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.button4.Location = new System.Drawing.Point(96, 190);
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.Transparent;
+            this.button4.Location = new System.Drawing.Point(534, 150);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(179, 94);
-            this.button4.TabIndex = 2;
+            this.button4.Size = new System.Drawing.Size(230, 122);
+            this.button4.TabIndex = 37;
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // button5
             // 
-            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.button2.Location = new System.Drawing.Point(327, 64);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(179, 94);
-            this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = true;
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(276, 150);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(230, 122);
+            this.button5.TabIndex = 36;
+            this.button5.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // button6
             // 
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.button1.Location = new System.Drawing.Point(96, 64);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(179, 94);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(16, 150);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(230, 122);
+            this.button6.TabIndex = 35;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 71);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(779, 209);
+            this.groupBox2.TabIndex = 38;
+            this.groupBox2.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(748, 168);
+            this.dataGridView1.TabIndex = 40;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 19);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "우리 도서관 공지사항";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Normal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(185)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1069, 574);
-            this.Controls.Add(this.group_mode);
+            this.ClientSize = new System.Drawing.Size(802, 579);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.picture_support);
+            this.Controls.Add(this.picture_logout);
             this.Controls.Add(this.label5);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Normal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "일반 사용자 모드 (일이삼사 도서관)";
-            this.group_mode.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picture_support)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_logout)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -172,13 +226,17 @@ namespace FINAL_project_LibraryProgram_1234
         #endregion
 
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.GroupBox group_mode;
+        private System.Windows.Forms.PictureBox picture_support;
+        private System.Windows.Forms.PictureBox picture_logout;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
     }
 }
