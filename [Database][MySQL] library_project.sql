f2f2e67dcd96c703e2c005e670bd854d2b1e7ce1
MinHyuk-Lee <m_file@naver.com>
2022-11-30 02:03:46 +0900

    [BACKUP] Backup Of MySQL DB File

--- [Database][MySQL] library_project.sql
+++ [Database][MySQL] library_project.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `library_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `library_project`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: localhost    Database: library_project
@@ -9,13 +7,21 @@ USE `library_project`;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `library_project`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `library_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `library_project`;
 
 --
 -- Table structure for table `board_free`
@@ -89,7 +95,7 @@ CREATE TABLE `board_qna` (
 
 LOCK TABLES `board_qna` WRITE;
 /*!40000 ALTER TABLE `board_qna` DISABLE KEYS */;
-INSERT INTO `board_qna` VALUES ('test','test','m_file@naver.com','test','test','test입니다.d'),('test2','test2','m_file@naver.com','test2','test2','test2885'),('test4','test4','m_file@naver.com','test4','test3','test3');
+INSERT INTO `board_qna` VALUES ('2211253659','이민혁','m_file@naver.com','test6','test6',NULL);
 /*!40000 ALTER TABLE `board_qna` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -127,6 +133,30 @@ INSERT INTO `book` VALUES ('014319251122','트렌드 코리아 2023','9788959897
 UNLOCK TABLES;
 
 --
+-- Table structure for table `master`
+--
+
+DROP TABLE IF EXISTS `master`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `master` (
+  `아이디` varchar(10) NOT NULL,
+  `비밀번호` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`아이디`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `master`
+--
+
+LOCK TABLES `master` WRITE;
+/*!40000 ALTER TABLE `master` DISABLE KEYS */;
+INSERT INTO `master` VALUES ('asdf','asdf'),('test','test'),('test2','test2'),('test3','test3');
+/*!40000 ALTER TABLE `master` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `member`
 --
 
@@ -156,17 +186,9 @@ CREATE TABLE `member` (
 
 LOCK TABLES `member` WRITE;
 /*!40000 ALTER TABLE `member` DISABLE KEYS */;
-INSERT INTO `member` VALUES ('111111111112','테스트','test','test','여자','경기도','010-0000-0001','1900-01-01','test@test.com','2000-01-01','2','정상'),('111111111112','테스트','test3','test3','여자','경기도','010-0000-0001','1900-01-01','test@test.com','2000-01-01','2','정상'),('2211253659','이민혁','min3659111','test','남자','테스트','010-0000-0000','1999-05-19','m_file@naver.com','2022-11-25','0','정상');
+INSERT INTO `member` VALUES ('2211253659','이민혁','min3659111','test','남자','테스트','010-0000-0000','1999-05-19','m_file@naver.com','2022-11-25','0','정상');
 /*!40000 ALTER TABLE `member` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'library_project'
---
-
---
--- Dumping routines for database 'library_project'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -177,4 +199,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-29 15:18:21
+-- Dump completed on 2022-11-30  2:01:03
